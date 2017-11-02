51903321812e76300838b4715f79c667cb185e10
srivaths <swetha.blue.sky@gmail.com>
2017-11-02 12:25:37 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-console.log("Up and Running");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne=cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo=cards[2];
+cardsInPlay.push(cardTwo);
+if(cardsInPlay.length===2)
+{
+  if(cardOne===cardTwo)
+  {
+    alert("You found a match");
+  }
+  else
+  {
+    alert("Sorry.Try again!")
+  }
+}
